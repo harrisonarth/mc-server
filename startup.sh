afd6584af111a727ea6905b58b5fc7da90545910
harrisonarth <arth.harrison@gmail.com>
2024-10-23 00:16:27 -0500

    more changes

--- startup.sh
+++ startup.sh
@@ -14,11 +14,19 @@ PACKAGES=(
     "vim"
 )
 
+DOCKPACKS=(
+    "docker-ce"
+    "docker-ce-cli"
+    "containerd.io"
+    "docker-buildx-plugin"
+    "docker-compose-plugin"
+)
+
 # Install list of packages
 echo "Installing packages..."
 for package in "${PACKAGES[@]}"; do
     echo "Installing $package..."
-    sudo dnf install -y "$package"
+    sudo dnf install -y "$package" >> logs.txt
 done
 echo "All packages have been installed."
 
@@ -32,7 +40,10 @@ echo "SSH enabled!"
 # Install docker
 echo "Downloading and installing Docker..."
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo >> logs.txt
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y >> logs.txt
+for dockpack in "${DOCKPACKS[@]}"; do
+    echo "Installing $dockpack..."
+    sudo dnf install -y "$dockpack" >> logs.txt
+done
 echo "Docker install complete!"
 
 # Start docker
@@ -43,13 +54,13 @@ sudo systemctl status docker >> logs.txt
 echo "Docker enabled!"
 
 # Run docker as a nonroot user
-echo "Configuring docker to run docker as a nonroot user"
-sudo usermod -aG docker $USER
-newgrp docker
-echo "Configuration complete!"
+#echo "Configuring docker to run docker as a nonroot user"
+#sudo usermod -aG docker $USER
+#newgrp docker
+#echo "Configuration complete!"
 
 # Install and start portainer
 echo "Installing portainer..."
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.3
+sudo docker volume create portainer_data >> logs.txt
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.3 >> logs.txt
 echo "Portainer started."
